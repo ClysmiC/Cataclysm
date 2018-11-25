0904693ccb10f713fcefdd20afc0a916bdf92579
ClysmiC <asmith379@gatech.edu>
2018-11-25 09:05:18 -0800

    seperate move and gravity

--- code/ecs/systems/MovementSystem.cpp
+++ code/ecs/systems/MovementSystem.cpp
@@ -25,182 +25,121 @@ void walkAndCamera(Game* game)
     EntityDetails* details = getEntityDetails(game->player);
     AgentComponent* agent = getAgentComponent(game->player);
 
-    assert((details->flags & EntityFlag_Static) == 0); // only dynamic object can walk!
-
     TransformComponent* camXfm = getTransformComponent(game->activeCamera);
-
+    
     float32 deltaTS = deltaTMs / 1000.0f;
 
-    //Vec3 posBeforeMove = xfm->position();
-    
-    Plane movementPlane(Vec3(0, 0, 0), Vec3(0, 1, 0));
-
-    Vec3 moveRight   = normalize(project(camXfm->right(), movementPlane));
-    Vec3 moveLeft    = -moveRight;
-    Vec3 moveForward = normalize(project(camXfm->forward(), movementPlane));
-    Vec3 moveBack    = -moveForward;
-
-    assert(FLOAT_EQ(moveRight.y, 0, 0.001));
-    assert(FLOAT_EQ(moveForward.y, 0, 0.001));
-
-    const float32 stickDeadzone = 0.05;
-    const float32 playerAccel = 35;
-    const float32 friction = 4;
-    const float gravity = 9.8;
-
-    Vec2 movementInput = moveRight.xz() * (abs(leftJoyX) >= stickDeadzone ? leftJoyX : 0) + 
-                    moveForward.xz() * (abs(leftJoyY) >= stickDeadzone ? leftJoyY : 0);
-
-    if (length(movementInput) > 1) movementInput.normalizeInPlace();
-
-    Vec2 acceleration = playerAccel * movementInput;
-    acceleration -= friction * agent->velocity;
-
-    Vec3 accelVec3 = Vec3(acceleration.x, 0, acceleration.y);
-    Vec3 velocityVec3 = Vec3(agent->velocity.x, 0, agent->velocity.y);
-    xfm->setPosition(xfm->position() + 0.5 * accelVec3 * deltaTS * deltaTS + velocityVec3 * deltaTS);
-
-    float32 yaw = TO_DEG(atan2(-agent->velocity.y, agent->velocity.x));
-    xfm->setOrientation(axisAngle(Vec3(0, 1, 0), yaw - 90));
-
-    agent->velocity += acceleration * deltaTS;
-
-    // float32 playerSpeed = length(physics->velocity);
-    //if (playerSpeed > maxPlayerSpeed)
-    //{
-    //    physics->velocity /= playerSpeed;    // normalize
-    //    physics->velocity *= maxPlayerSpeed; // set to max
-    //}
-
-    // Friction
-    //physics->velocity -= normalizeOrZero(physics->velocity) * friction * deltaTS;
-    
-
-    //bool draggingCameraInEditMode =
-    //    game->editor.isEnabled &&
-    //    !game->editor.translator.isHandleSelected &&
-    //    mouseButtons[GLFW_MOUSE_BUTTON_1] &&
-    //    !ImGui::GetIO().WantCaptureMouse;
-
-    // TODO: guard this if in editor mode?
-    //if (mouseXPrev != FLT_MAX && mouseYPrev != FLT_MAX)
-    //{
-    //    // Rotate
-    //    float32 deltaMouseX = mouseX - mouseXPrev;
-    //    float32 deltaMouseY = mouseY - mouseYPrev;
-
-    //    if (draggingCameraInEditMode)
-    //    {
-    //        // Drag gesture moves in opposite direction
-    //        deltaMouseX = -deltaMouseX;
-    //        deltaMouseY = -deltaMouseY;
-    //    }
-
-    //    Quaternion deltaYaw;
-    //    Quaternion deltaPitch;
-    //    deltaYaw = axisAngle(Vec3(0, 1, 0), cameraTurnSpeed * -deltaMouseX * deltaTS); // yaw
-    //    deltaPitch = axisAngle(Vec3(1, 0, 0), cameraTurnSpeed * deltaMouseY * deltaTS); // pitch
-
-    //    // "Player" just yaws.
-    //    // Camera yaws and pitches
-    //    xfm->setOrientation(deltaYaw * xfm->orientation());
-    //}
-
-    //if (keys[GLFW_KEY_W])
-    //{
-    //    xfm->setLocalPosition(xfm->localPosition() + moveForward * cameraSpeed * deltaTS);
-    //}
-    //else if (keys[GLFW_KEY_S])
-    //{
-    //    xfm->setLocalPosition(xfm->localPosition() + moveBack * cameraSpeed * deltaTS);
-    //}
-    //    
-    //if (keys[GLFW_KEY_A])
-    //{
-    //    xfm->setLocalPosition(xfm->localPosition() + moveLeft * cameraSpeed * deltaTS);
-    //}
-    //else if (keys[GLFW_KEY_D])
-    //{
-    //    xfm->setLocalPosition(xfm->localPosition() + moveRight * cameraSpeed * deltaTS);
-    //}
-
     //
-    // Resolve collision
+    // Begin lambda
     //
-    if (collider)
+    auto resolveCollisions = [&]()
     {
-        FOR_BUCKET_ARRAY (game->activeScene->ecs.colliders.components)
+        bool collided = false;
+
+        if (collider)
         {
-            // @Slow. Improve with spatial partitioning
-            ColliderComponent* cc = it.ptr;
-            EntityDetails* debug_details = getEntityDetails(cc->entity);
-
-            if (cc->isTrigger || cc->entity.id == game->player.id) continue;
-
-            GjkResult collisionResult = gjk(collider, cc);
-
-            if (collisionResult.collides)
+            FOR_BUCKET_ARRAY(game->activeScene->ecs.colliders.components)
             {
-                xfm->setPosition(xfm->position() - collisionResult.penetrationVector);
-                //physics->velocity -= project(physics->velocity, normalize(collisionResult.penetrationVector));
+                // @Slow. Improve with spatial partitioning
+                ColliderComponent* cc = it.ptr;
+                EntityDetails* debug_details = getEntityDetails(cc->entity);
+
+                if (cc->isTrigger || cc->entity.id == game->player.id) continue;
+
+                GjkResult collisionResult = gjk(collider, cc);
+
+                if (collisionResult.collides)
+                {
+                    collided = true;
+                    xfm->setPosition(xfm->position() - collisionResult.penetrationVector);
+                    //physics->velocity -= project(physics->velocity, normalize(collisionResult.penetrationVector));
+                }
+            }
+
+            FOR_BUCKET_ARRAY(game->activeScene->ecs.convexHullColliders.components)
+            {
+                // @Slow. Improve with spatial partitioning
+                ConvexHullColliderComponent* cc = it.ptr;
+                EntityDetails* debug_details = getEntityDetails(cc->entity);
+
+                if (cc->isTrigger || cc->entity.id == game->player.id) continue;
+
+                GjkResult collisionResult = gjk(collider, cc);
+
+                if (collisionResult.collides)
+                {
+                    collided = true;
+                    xfm->setPosition(xfm->position() - collisionResult.penetrationVector);
+                }
             }
         }
 
-        FOR_BUCKET_ARRAY (game->activeScene->ecs.convexHullColliders.components)
-        {
-            // @Slow. Improve with spatial partitioning
-            ConvexHullColliderComponent* cc = it.ptr;
-            EntityDetails* debug_details = getEntityDetails(cc->entity);
+        return collided;
+    };
 
-            if (cc->isTrigger || cc->entity.id == game->player.id) continue;
+    //
+    // End lambda
+    //
 
-            GjkResult collisionResult = gjk(collider, cc);
+    assert((details->flags & EntityFlag_Static) == 0); // only dynamic object can walk!
+    
+    //
+    // Move (w/o gravity)
+    //
+    {
+        Plane movementPlane(Vec3(0, 0, 0), Vec3(0, 1, 0));
 
-            if (collisionResult.collides)
-            {
-                xfm->setPosition(xfm->position() - collisionResult.penetrationVector);
-            }
-        }
+        Vec3 moveRight = normalize(project(camXfm->right(), movementPlane));
+        Vec3 moveLeft = -moveRight;
+        Vec3 moveForward = normalize(project(camXfm->forward(), movementPlane));
+        Vec3 moveBack = -moveForward;
+
+        assert(FLOAT_EQ(moveRight.y, 0, 0.001));
+        assert(FLOAT_EQ(moveForward.y, 0, 0.001));
+
+        const float32 stickDeadzone = 0.05;
+        const float32 playerAccel = 35;
+        const float32 friction = 4;
+
+        Vec2 movementInput = moveRight.xz() * (abs(leftJoyX) >= stickDeadzone ? leftJoyX : 0) +
+            moveForward.xz() * (abs(leftJoyY) >= stickDeadzone ? leftJoyY : 0);
+
+        if (length(movementInput) > 1) movementInput.normalizeInPlace();
+
+        Vec2 acceleration = playerAccel * movementInput;
+        acceleration -= friction * agent->velocity;
+
+        xfm->setPosition(xfm->position() + 0.5 * Vec3(acceleration.x, 0, acceleration.y) * deltaTS * deltaTS + Vec3(agent->velocity.x, 0, agent->velocity.y) * deltaTS);
+
+        resolveCollisions();
+
+        agent->velocity += acceleration * deltaTS;
+        float32 yaw = TO_DEG(atan2(-agent->velocity.y, agent->velocity.x));
+        xfm->setOrientation(axisAngle(Vec3(0, 1, 0), yaw - 90));
     }
 
     //
-    // Snap to ground
+    // Apply gravity
     //
-    //if (terrain)
-    //{
-    //    float32 height = getTerrainHeight(terrain, xfm->position().x, xfm->position().z);
+    {
+        const float32 gravity = -9.8;
+        const float32 drag = 1;
 
-    //    xfm->setPosition(
-    //        Vec3(
-    //            xfm->position().x,
-    //            height,
-    //            xfm->position().z
-    //        )
-    //    );
-    //}
+        float32 gravAccel = gravity - drag * agent->yVelocity;
+        Vec3 gravAccelVector = Vec3(0, gravAccel, 0);
+        Vec3 gravVelVector = Vec3(0, agent->yVelocity, 0);
 
-    //
-    // Go thru portal
-    //
-    //FOR_BUCKET_ARRAY (game->activeScene->ecs.portals.components)
-    //{
-    //    PortalComponent* pc = it.ptr;
-    //    if (pc->connectedPortal.id == 0) continue;
-    //    
-    //    ColliderComponent* cc = getColliderComponent(pc->entity);
+        xfm->setPosition(xfm->position() + 0.5 * gravAccelVector * deltaTS * deltaTS + gravVelVector * deltaTS);
 
-    //    if (pointInsideCollider(cc, xfm->position()))
-    //    {
-    //        Vec3 portalPos = getTransformComponent(pc->entity)->position();
-    //        Vec3 portalToOldPos = posBeforeMove - portalPos;
-    //        Vec3 portalToPos = xfm->position() - portalPos;
-    //        
-    //        if (dot(portalToOldPos, outOfPortalNormal(pc)) >= 0)
-    //        {
-    //            rebaseTransformInPlace(pc, xfm);
-    //            EntityDetails* connectedPortal = getEntityDetails(getEntity(getGame(pc->entity), &pc->connectedPortal));
-    //            game->activeScene = connectedPortal->entity.ecs->scene;
-    //        }
-    //    }
-    //}
+        if (resolveCollisions())
+        {
+            agent->isGrounded = true;
+            agent->yVelocity = 0;
+        }
+        else
+        {
+            agent->isGrounded = false;
+            agent->yVelocity += gravAccel * deltaTS;
+        }
+    }
 }
